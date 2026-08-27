f2668851e093bf76e3e72dd5d2e978cd7cb1b1b0
mab-120 <milenaaraujobarbosa120@gmail.com>
2026-08-27 19:18:43 -0300

    arrumando 2

---
+++ RH-TCC-main/TCC-ERP-DashboardExecutivo/appClientModule/app/Main.java
@@ -0,0 +1,24 @@
+import TelaDashboard;
+import javax.swing.*;
+
+
+public class Main {
+
+	  public static void main(String[] args) {
+	    
+	    SwingUtilities.invokeLater(
+	      new Runnable() {
+	        public void run() {
+	          try {
+	            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+	          } catch (Exception e) {
+	            
+	            e.printStackTrace();
+	          }
+	          new TelaDashboard().setVisible(true);
+	        }
+	      }
+	    );
+	  }
+	}
